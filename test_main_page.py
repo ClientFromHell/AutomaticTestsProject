de136e0854e853335e5b385c334dd0f721cda592
ClientFromHell <s.zheludev@gmail.com>
2022-09-19 16:00:11 +0300

    Exceptions through assert

--- test_main_page.py
+++ test_main_page.py
@@ -20,8 +20,16 @@ from selenium.webdriver.common.by import By
 #     browser.get(link)
 #     go_to_login_page(browser)
 
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
