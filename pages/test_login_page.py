b67751e46f9e6248f7ec08f87bf296dc076bbfdf
ClientFromHell <s.zheludev@gmail.com>
2022-09-20 08:59:31 +0300

    Added test for login page

---
+++ pages/test_login_page.py
@@ -0,0 +1,12 @@
+from .login_page import LoginPage
+from .main_page import MainPage
+
+
+def test_if_login_page_has_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.open()
+    login_page.should_be_login_page()
