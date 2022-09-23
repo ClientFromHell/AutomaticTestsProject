adf2b44b62b60f6367089f444620ce88b98de278
ClientFromHell <s.zheludev@gmail.com>
2022-09-23 18:46:04 +0300

    Product Page tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
-from .product_page import ProductPage
-from .locators import ProductPageLocators
+from .pages.product_page import ProductPage
+from .pages.locators import ProductPageLocators
+from .pages.basket_page import BasketPage
 import pytest
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -58,7 +60,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page (browser):
     url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, url)
@@ -66,8 +68,9 @@ def test_guest_can_go_to_login_page_from_product_page (browser):
     page.go_to_login_page()
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     url = 'https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/'
-    page = ProductPage(browser, url)
+    page = BasketPage(browser, url)
     page.open()
     page.go_to_basket_and_check_if_it_is_empty()
