7602ade688f5b1187ad2196cb3d372364781a4fa
ClientFromHell <s.zheludev@gmail.com>
2022-09-23 17:19:18 +0300

    Moved test_main_page into ../ folder

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,22 @@
-from .pages.main_page import MainPage
-from selenium.webdriver.common.by import By
-from time import sleep
+from .main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
     page.open()
     page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, url)
+    page.open()
+    page.go_to_basket_and_check_if_it_is_empty()
