712841dded4014bd5df8f1af91a0f7c0669fbbb1
ClientFromHell <s.zheludev@gmail.com>
2022-09-23 17:05:42 +0300

    Product Page tests

--- pages/test_product_page.py
+++ pages/test_product_page.py
@@ -1,4 +1,5 @@
 from .product_page import ProductPage
+from .locators import ProductPageLocators
 import pytest
 
 
@@ -24,3 +25,47 @@ def test_guest_can_add_product_to_basket(browser, promo):
     page.add_product_to_basket()
     page.the_product_name_is_the_same()
     page.the_price_name_is_the_same()
+
+
+def test_guest_cant_see_success_message(browser):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    url = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+    page = ProductPage(browser, url)
+    page.open()
+    page.add_product_to_basket()
+    page.success_message_should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, url)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page (browser):
+    url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    url = 'https://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/'
+    page = ProductPage(browser, url)
+    page.open()
+    page.go_to_basket_and_check_if_it_is_empty()
